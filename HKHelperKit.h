dcbc555fedd723c1b0a0b5a98e08ddeb589278b7
Alex Wayne <alex@beautifulpixel.com>
2010-02-17 17:58:41 -0800

    initial commit with some simple kit extensions

---
+++ HKHelperKit.h
@@ -0,0 +1,12 @@
+//
+//  HKHelperKit.h
+//  HelperKit
+//
+//  Created by Alex Wayne on 2/16/10.
+//  Copyright 2010 Beautiful Pixel. All rights reserved.
+//
+
+#import "NSString+HelperKit.h"
+
+#import "UIBarButtonItem+HelperKit.h"
+#import "UIImage+HelperKit.h"
