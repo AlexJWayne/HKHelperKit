be173d9638f33cb2872f9c42fa050a7f30079aa9
Alex Wayne <alex@beautifulpixel.com>
2010-03-25 20:21:10 -0700

    added property versions of many NSURLRequest methods

--- TestSuite/Tests/TestNSURLRequest.m
+++ TestSuite/Tests/TestNSURLRequest.m
@@ -12,9 +12,44 @@
 @implementation TestNSURLRequest
 
 - (void)tests {
+    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURLString:@"http://google.com"];
+    
     [self assert:@"[NSURLRequest requestWithURLString:] should create a new request from a string" do:^{
-        NSURLRequest *req = [NSURLRequest requestWithURLString:@"http://google.com"];
-        return (BOOL)([[req URL] isEqual:[NSURL URLWithString:@"http://google.com"]]);
+        NSURLRequest *request = [NSURLRequest requestWithURLString:@"http://google.com"];
+        return (BOOL)([[request URL] isEqual:[NSURL URLWithString:@"http://google.com"]]);
+    }];
+    
+    [self assert:@"cachePolicy should be a property" do:^{
+        [req setCachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData];
+        NSURLRequestCachePolicy expected = [req cachePolicy];
+        return (BOOL)(expected == req.cachePolicy);
+    }];
+    
+    [self assert:@"httpBody should be a property" do:^{
+        [req setHTTPBody:[@"foobar" data]];
+        return (BOOL)([req.httpBody isEqualToString:@"foobar"]);
+    }];
+    
+    [self assert:@"httpMethod should be a property" do:^{
+        [req setHTTPMethod:@"POST"];
+        return (BOOL)([req.httpMethod isEqualToString:@"POST"]);
+    }];
+    
+    [self assert:@"shouldHandleCookies should be a property" do:^{
+        [req setHTTPShouldHandleCookies:NO];
+        return (BOOL)(!req.shouldHandleCookies);
+    }];
+    
+    [self assert:@"url should be a property" do:^{
+        NSString *expected = [[NSURL URLWithString:@"http://google.com"] absoluteString];
+        NSString *actual = [req.url absoluteString];
+        return (BOOL)([expected isEqualToString:actual]);
+    }];
+    
+    [self assert:@"timeoutInterval should be a property" do:^{
+        int expected = [req timeoutInterval];
+        int actual = req.timeoutInterval;
+        return (BOOL)(expected == actual);
     }];    
 }
 
